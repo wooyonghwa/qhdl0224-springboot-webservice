058a1e7fd393a396436adb0b165469973fe2d1fb
yonghwa <qhdl0224@gmail.com>
2020-07-12 17:07:50 +0900

    소셜 로그인 추가(구글,네이버)

--- src/main/java/com/qhdl0224/book/springboot/web/IndexController.java
+++ src/main/java/com/qhdl0224/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.qhdl0224.book.springboot.web;
 
+import com.qhdl0224.book.springboot.config.auth.LoginUser;
+import com.qhdl0224.book.springboot.config.auth.dto.SessionUser;
 import com.qhdl0224.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,14 +9,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
